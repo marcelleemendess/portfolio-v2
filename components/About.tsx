294fccd4f8d6f7c2ac1a31b120157f21bc251519
Marcelle Mendes <62451375+marcelleemendess@users.noreply.github.com>
2022-04-21 18:00:57 -0600

    edit text

--- components/About.tsx
+++ components/About.tsx
@@ -25,7 +25,7 @@ const About = ({ toggleContent, setToggleContent}) => {
           exit="exit"
         >
           <h5 className="my-3 font-medium">
-            I a passionate <RoughNotationGroup show={true}><RainbowHighlight color={colors[0]}>Software Developer</RainbowHighlight></RoughNotationGroup>  who uses web technologies to create user-friendly, simple and engaging products.
+            I am passionate <RoughNotationGroup show={true}><RainbowHighlight color={colors[0]}>Software Developer</RainbowHighlight></RoughNotationGroup>  who uses web technologies to create user-friendly, simple and engaging products.
             I have spent most of my time working with  <RoughNotationGroup show={true}><RainbowHighlight color={colors[0]}>Javascript</RainbowHighlight></RoughNotationGroup>, the frameworks, and new features, but I am always excited to learn new technologies that challenge me. I would love to hear from you. Whether it's about a project or just a conversation. 
             <br/>
             <RoughNotationGroup show={true}><RainbowHighlight color={colors[0]}>Just get in touch with me. </RainbowHighlight></RoughNotationGroup>
